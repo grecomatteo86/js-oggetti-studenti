cb6d5a2f2c9831d4886ba1ab4b27acaf6e9f35b9
Matteo <grecomatteo86@gmail.com>
2021-03-17 16:21:19 +0100

    creazione array oggetti e stampa nome e cognome commit

--- js/script.js
+++ js/script.js
@@ -24,3 +24,50 @@ for ( var key in studenteAlfa ) {
   console.log(studenteAlfa[key]);
 
 }
+
+
+/* Creare un array di oggetti di studenti.
+Ciclare su tutti gli studenti
+e stampare per ognuno nome e cognome */
+
+// 1. Creo un array di oggetti di Studenti
+var arrayStudenti = [
+
+  {
+    nome : 'Andrea',
+    cognome : 'Vai',
+    eta : 33,
+    lavoro: 'idraulico'
+  },
+
+  {
+    nome : 'Luca',
+    cognome : 'Bianchi',
+    eta : 18,
+    lavoro: 'studente'
+  },
+
+  {
+    nome : 'Mario',
+    cognome : 'Verdi',
+    eta : 21,
+    lavoro: 'sviluppatore'
+  },
+
+  {
+    nome : 'Luigi',
+    cognome : 'Buongiorno',
+    eta : 25,
+    lavoro: 'imbianchino'
+  }
+
+];
+
+/* 2. Per ciclare su tutti gli studenti, cioè ciclare l'array,
+devo utilizzare un ciclo for */
+for ( var i = 0 ; i < arrayStudenti.length; i++) {
+
+  // Stampo per ognuno nome e cognome
+  console.log(arrayStudenti[i].nome + ' ' + arrayStudenti[i]['cognome']);
+
+}
